c5605bc41ae73cd15a46cc613b54606af8f6c43f
zjq <460631588@qq.com>
2012-05-05 20:45:35 +0800

    对 manage 方法进行一些实验性尝试。

--- conm/control/a_model.class.php
+++ conm/control/a_model.class.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * 2012-05-05
+ * 
+ * @package default
+ * @filesource
+ */
 class ctrl_a_model
 {
 	function index()
@@ -10,65 +16,76 @@ class ctrl_a_model
 	 */
 	function manage()
 	{//{{{
-		$infos = $model->listinfo('modeltype=0', 'modelid', 1, 100);
-		include admin_tpl('model_manage');
+		$sql = "SELECT * FROM " . RDB_PRE . "model";
+		$result = rdb::obj()->select($sql);
+		var_dump($result);
+		// $infos = $model->listinfo('modeltype=0', 'modelid', 1, 100);
+		// include admin_tpl('model_manage');
 	}//}}}
 	function add()
 	{//{{{
-		if($dosubmit)
-		{
+		if (isset($_POST["dosubmit"]))
+			{
 			$modelid = $model->add($info);
 			if($modelid)
-			{
+				{
 				//cache_model();
 				showmessage('操作成功！', admin_url(".model_field.manage.&modelid={$modelid}"));
-			}
+				}
 			else
-			{
+				{
 				showmessage('操作失败！');
+				}
 			}
-		}
 		else
-		{
-			include admin_tpl('model_add');
-		}
+			{
+			?>
+<pre>
+模型名：
+模型表名：
+模型类型：[下拉框]
+模型类型自己的设置表单，在选中模型类型后通过 ajax 加载出来。
+</pre>
+<?php
+			// include admin_tpl('model_add');
+			}
 	}//}}}
 	function edit()
 	{//{{{
 		if($dosubmit)
-		{
+			{
 			$result = $model->edit($modelid, $info);
 			if($result)
-			{
+				{
 				require_once 'admin/category.class.php';
 				$cat = new category('phpcms');
 				$cat->repair();
 				cache_model();
 
 				if(is_array($CATEGORY) && $ishtml != $info['ishtml'])
-				{
+					{
 					$forward = '?mod=phpcms&file=url'.$catids.'&forward='.urlencode(URL);
 					foreach($CATEGORY AS $k=>$v)
-					{
+						{
 						if($v['modelid'] != $modelid) continue;
 						showmessage('内容模型修改成功！请更新对应的栏目URL链接', $forward,'4000');
-					}
+						}
 
-				}
+					}
 				showmessage('操作成功！', $forward);
-			}
+				}
 			else
-			{
+				{
 				showmessage('操作失败！');
+				}
 			}
-		}
 		else
-		{
+			{
 			$info = $model->get($modelid);
 			if(!$info) showmessage('指定的模块不存在！');
 			extract($info);
 			include admin_tpl('model_edit');
-		}
+			}
 	}//}}}
 	function export()
 	{//{{{
@@ -80,29 +97,29 @@ class ctrl_a_model
 	function import()
 	{//{{{
 		if($dosubmit)
-		{
+			{
 			if(!$info['name']) showmessage('请输入模型名称');
 			if(!$info['tablename']) showmessage('请输入表名');
 			if(!class_exists('attachment'))
-			{
+				{
 				require 'attachment.class.php';
-			}
-			
+				}
+
 			$attachment = new attachment('phpcms');
 			$aid = $attachment->upload('modelfile', 'model');
 			if(!$aid) showmessage($attachment->error(), $forward);
 			$filepath = $attachment->get($aid[0], 'filepath');
-			
+
 			$array = include(UPLOAD_ROOT.$filepath['filepath']);
 			if(empty($array)) showmessage('上传模型的格式不正确');
 			$modelid = $model->import($info);
 			if(!$modelid) showmessage($model->msg, $forward);
 			if(is_array($array['arr_field']) && !empty($array['arr_field']) && $modelid)
-			{
+				{
 				$tablename = DB_PRE.'c_'.$info['tablename'];
 				$arr_model_field = array('contentid', 'catid', 'typeid', 'areaid', 'title', 'style', 'thumb', 'keywords', 'description', 'posids', 'listorder', 'url', 'userid', 'updatetime', 'inputtime', 'status', 'template', 'content', 'islink', 'prefix');
 				foreach($array['arr_field'] as $arr_field)
-				{
+					{
 					$arr_field['modelid'] = $modelid;
 					$arr_field = new_addslashes($arr_field);
 					$db->insert(DB_PRE.'model_field', $arr_field);
@@ -115,45 +132,45 @@ class ctrl_a_model
 					$excutefile = str_replace('<?php', '', $excutefile);
 					$excutefile = str_replace('?>', '', $excutefile);
 					eval($excutefile);
+					}
 				}
-			}
 			if(!class_exists('model_field'))
-			{
+				{
 				require 'admin/model_field.class.php';
-			}
+				}
 			$field = new model_field($modelid);
 			$field->cache();
 			$attachment->delete("aid='$aid[0]'");
 			showmessage('操作成功！', $forward);
-		}
+			}
 		else
-		{
+			{
 			include admin_tpl('model_import');
-		}
+			}
 	}//}}}
 	function delete()
 	{//{{{
 		$result = $model->delete($modelid);
 		if($result)
-		{
+			{
 			showmessage('操作成功！', $forward);
-		}
+			}
 		else
-		{
+			{
 			showmessage('操作失败！', $forward);
-		}
+			}
 	}//}}}
 	function disable()
 	{//{{{
 		$result = $model->disable($modelid, $disabled);
 		if($result)
-		{
+			{
 			showmessage('操作成功！', $forward);
-		}
+			}
 		else
-		{
+			{
 			showmessage('操作失败！');
-		}
+			}
 	}//}}}
 	function urlrule()
 	{//{{{
