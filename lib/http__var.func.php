9eb951a3de928a5f312c560f9eb9fbea360d5a17
zjq <460631588@qq.com>
2012-05-09 22:43:49 +0800

    新增 _gp, _pg 函数。

--- lib/http__var.func.php
+++ lib/http__var.func.php
@@ -11,6 +11,7 @@
 /**
  * 提取 $_GET 的数据
  * @param NULL|string 若为 NULL 表示返回整个 $_GET 数组，否则返回指定的值。
+ * @todo 原型应为 key, func_name(过滤函数且支持多个), default_value(默认值), default_type(使用默认值条件：empty, !isset)
  */
 function _g($key = NULL)
 {//{{{
@@ -32,4 +33,28 @@ function _p($key = NULL)
 		return $_POST;
 		}
 	return $_POST[$key];
+}//}}}
+
+/**
+ * 从 GET, POST 中提取数据， GET 优先。
+ */
+function _gp($key)
+{//{{{
+	if (isset($_GET[$key]))
+		{
+		return $_GET[$key];
+		}
+	return $_POST[$key];
+}//}}}
+
+/**
+ * 从 POST, GET 中提取数据， POST 优先。
+ */
+function _pg($key)
+{//{{{
+	if (isset($_POST[$key]))
+		{
+		return $_POST[$key];
+		}
+	return $_GET[$key];
 }//}}}
