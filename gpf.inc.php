4d876b0ad8cfd2f42e9cdd9297cb8284b845a886
zjq <460631588@qq.com>
2012-06-23 22:29:53 +0800

    更改变量名。
    把GPF框架目录移到sour目录上一级。现在与sour同级。

--- gpf.inc.php
+++ gpf.inc.php
@@ -10,28 +10,28 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 // 入口文件必须定义常量：
 // *因为 gencms 并不知道项目的名称，因此如数据目录需要在入口文件中定义。
-// $ds = DIRECTORY_SEPARATOR;$dir = dirname(__FILE__);$pdir = dirname($dir);$name = 'project_name';
-// define('G_PATH_INST', $dir . $ds); //项目目录。
-// define('G_PATH_DATA', "{$pdir}{$ds}{$name}_data{$ds}"); //数据目录。
-// define('G_PATH_UPLOADFILE', "{$pdir}{$ds}{$name}_uploadfile{$ds}"); //上传文件保存目录。
-// define('G_PATH_CACHE', "{$pdir}{$ds}{$name}_cache{$ds}"); //缓存文件目录。
-// unset($ds, $dir, $pdir, $name);
+// $ds = DIRECTORY_SEPARATOR;$dir = dirname(__FILE__);
+// define('GPF_PATH_SOUR', "{$dir}{$ds}sour{$ds}"); //源目录(系统模块)
+// define('GPF_PATH_INST', "{$dir}{$ds}inst{$ds}"); //副本目录(项目模块)。
+// define('G_PATH_DATA', "{$dir}{$ds}data{$ds}"); //数据目录。
+// define('G_PATH_UPLOADFILE', "{$dir}{$ds}uploadfile{$ds}"); //上传文件保存目录。
+// define('G_PATH_CACHE', "{$dir}{$ds}cache{$ds}"); //缓存文件目录。
+// include "{$dir}{$ds}gpf{$ds}gpf.inc.php";
+// unset($ds, $dir);
 
 define('DS', DIRECTORY_SEPARATOR);
 //路径常量用 G_PATH_ 前序。
-define('G_PATH_GPF', dirname(__FILE__) . DS); //gpf 目录
-define('G_PATH_GPF_CORE', G_PATH_GPF . 'core' . DS); //gpf core 目录
-define('G_PATH_GPF_LIB', G_PATH_GPF . 'lib' . DS); //gpf lib 目录
-define('G_PATH_MOD_SOUR', dirname(G_PATH_GPF) . DS); //系统模块目录
-define('G_PATH_MOD_INST', G_PATH_INST); //项目模块目录
+define('GPF_PATH', dirname(__FILE__) . DS); //gpf 目录
+define('GPF_PATH_CORE', GPF_PATH . 'core' . DS); //gpf core 目录
+define('GPF_PATH_LIB', GPF_PATH . 'lib' . DS); //gpf lib 目录
 
 //加载配置文件
-require G_PATH_GPF . "config.inc.php";
+require GPF_PATH . "config.inc.php";
 
 //load core
-require G_PATH_GPF_CORE . "module.func.php";
-require G_PATH_GPF_CORE . "log.class.php";
-require G_PATH_GPF_CORE . "gpf.class.php";
+require GPF_PATH_CORE . "module.func.php";
+require GPF_PATH_CORE . "log.class.php";
+require GPF_PATH_CORE . "gpf.class.php";
 
 //load lib
-include G_PATH_GPF_LIB . "common.func.php";
+include GPF_PATH_LIB . "common.func.php";
